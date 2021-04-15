3d34d4029d9752f23933e405cd8d335d707a98c0
Francisco <javiermartincuenca@gmail.com>
2021-04-15 22:04:56 +0200

    Search by collection

--- backend/src/controllers/search.controller.ts
+++ backend/src/controllers/search.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
 import { Doctor, Hospital, User } from 'models';
-import { GetSearchResponse } from 'responses/search.response';
+import {
+  GetSearchByCollectionResponse,
+  GetSearchResponse,
+} from 'responses/search.response';
 
 export async function getSearch(
   req: Request<{ criteria: string }>,
@@ -40,30 +43,41 @@ export async function getSearch(
 
 export async function getSearchByCollection(
   req: Request<{ collection: string; criteria: string }>,
-  res: Response<GetSearchResponse>
+  res: Response<GetSearchByCollectionResponse>
 ) {
+  let result = undefined;
   const regex = new RegExp(req.params.criteria, 'i');
 
-  // switch(req.params.collection){
-  //   case 'hospitals':
-  //     break;
-  //     case 'users':
-  //       break;
-  //       case 'doctors':
-  //         break;
-  //         default:
-  // }
-  // const [users, doctors, hospitals] = await Promise.all([
-  //   User.find({ name: regex }),
-  //   Doctor.find({ name: regex }),
-  //   Hospital.find({ name: regex }),
-  // ]);
+  try {
+    switch (req.params.collection) {
+      case 'hospitals':
+        result = await Hospital.find({ name: regex });
+        break;
+      case 'users':
+        result = await User.find({ name: regex });
+        break;
+      case 'doctors':
+        result = await Doctor.find({ name: regex });
+        break;
+      default:
+        res.status(400).json({
+          ok: false,
+          message:
+            'Collection should be one of "users", "hospitals" or "doctors".',
+        });
+    }
 
-  // res.json({
-  //   ok: true,
-  //   message: 'Users list',
-  //   users,
-  //   hospitals,
-  //   doctors,
-  // });
+    if (result) {
+      res.json({
+        ok: true,
+        message: `${req.params.collection} results with criteria ${req.params.criteria}`,
+        result,
+      });
+    }
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: `Unexpected error: ${error.toString()}`,
+    });
+  }
 }
