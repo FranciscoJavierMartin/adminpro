5836ac749eed92891a6820ac676ab378aaa270fb
Francisco <javiermartincuenca@gmail.com>
2021-04-15 10:26:09 +0200

    Added Morgan to log requests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 import { config } from 'dotenv';
 import { connectToDB } from 'database/config';
 import cors from 'cors';
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('tiny'));
 
 connectToDB();
 
